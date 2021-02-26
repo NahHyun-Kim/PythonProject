df85e025838576ba890e8aafaa6590d1c98fca6c
NahHyun-Kim <cherish3_@naver.com>
2021-02-27 00:24:49 +0900

    codeit python(while)

---
+++ CodeitPython/pythonBasic/while.py
@@ -0,0 +1,20 @@
+# while문(초기화, while문 조건: 출력할 문장, 증감식)
+i = 1
+
+#  while True: 사용시 무한루프로 출력 가능
+while i <= 3:
+    print("I can code too!")
+    i = i + 1
+
+# 100 이상의 자연수 중 가장 작은 23의 배수 출력
+j = 100
+while True: # while True: 혹은 while 1: 사용 가능(우선 계속 실행)
+    if j % 23 == 0:
+        print(f"{j}")
+        break
+    j = j + 1
+
+k = 100
+while k % 23 != 0:
+    k = k + 1
+print(k)
